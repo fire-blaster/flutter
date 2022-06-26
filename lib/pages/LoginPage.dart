0303fa6fec8f1f7253b12793a4ff871502c6aa26
Bambhaniya Sumit <9067016443s@gmail.com>
2022-06-26 22:08:14 +0530

    Gradient in login page

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -19,83 +19,145 @@ class _LoginPageState extends State<LoginPage> {
         centerTitle: true,
         title: const Text("Login"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              const Text("User Name"),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: userName,
-                decoration: InputDecoration(
-                  hintText: "Enter User Name",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(),
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color.fromARGB(255, 195, 0, 255),
+                  Color.fromARGB(255, 255, 5, 9),
+                ]),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: [
+                  const Text("User Name"),
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
+                  TextFormField(
+                    controller: userName,
+                    decoration: InputDecoration(
+                      hintText: "Enter User Name",
+                      hintStyle: const TextStyle(
+                        color: Colors.amber,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
 
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter User Name';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text("Password"),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                obscureText: true,
-                controller: password,
-                decoration: InputDecoration(
-                  hintText: "Enter Password",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter User Name';
+                      }
+                      return null;
+                    },
                   ),
-                ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text("Password"),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    controller: password,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      hintStyle: const TextStyle(
+                        color: Colors.amber,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
 
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter Password';
-                  }
-                  return null;
-                },
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 130.0),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            // side: BorderSide(color: Colors.white, ),
+                            side: const BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // ScaffoldMessenger.of(context).showSnackBar(
+                          //   const SnackBar(content: Text('Processing Data')),
+                          // );
+                          print(
+                              "User Name:-${userName.text}, Password:-${password.text}");
+                          Map userRegisterData = {
+                            "username": userName.text,
+                            "password": password.text,
+                          };
+                          Navigator.pushNamed(context, "/home");
+                        } else {
+                          print("Invalid");
+                        }
+                      },
+                      // shape: RoundedRectangleBorder(
+                      //   borderRadius: BorderRadius.circular(80.0),
+                      // ),
+
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          // backgroundColor: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // ScaffoldMessenger.of(context).showSnackBar(
-                    //   const SnackBar(content: Text('Processing Data')),
-                    // );
-                    // print(
-                    //     "User Name:-${userName.text}, Password:-${password.text}");
-                    Map userRegisterData = {
-                      "username": userName.text,
-                      "password": password.text,
-                    };
-                    Navigator.pushNamed(context, "/home");
-                  } else {
-                    // print("Invalid");
-                  }
-                },
-                child: const Text('Login'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
